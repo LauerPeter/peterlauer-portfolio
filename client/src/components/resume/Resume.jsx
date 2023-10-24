873adebf2f086bc2bbe25aa0f274292e3aec8a09
Peter Lauer <lauerpeter11@gmail.com>
2023-10-24 16:48:28 -0500

    removed download button

--- client/src/components/resume/Resume.jsx
+++ client/src/components/resume/Resume.jsx
@@ -2,23 +2,7 @@
 
 import React, { useRef } from 'react';
 import './Resume.css';
-import docxFile from './resume.docx'
 
-class DocxDownload extends React.Component {
-  handleDownload = () => {
-    try {
-      window.open(docxFile, '_blank');
-    } catch (error) {
-      console.error('Error downloading DOCX file:', error);
-    }
-  };
-
-  render() {
-    return (
-      <button onClick={this.handleDownload}>Download DOCX</button>
-    );
-  }
-}
 
 function Resume() {
 
@@ -27,73 +11,66 @@ function Resume() {
   return (
     <div className='resume-flex-container'>
       <div className='resume-container' style={{ backgroundColor: 'white'}} ref={pdfContainerRef}>
-      
-        <h1 className='resumeHeader'>Peter Lauer</h1>
+        <h1 className='resumeHeader'>PETER LAUER</h1>
         <div className='info'>
-          <p> | Minneapolis, MN |
-            LinkedIn: <a href="https://www.linkedin.com/in/peter-lauer-7898aa276/"> /PeterLauerLinkedIn</a> | 
-            GitHub: <a href="https://github.com/LauerPeter"> /PeterLauerGitHub</a> | 
+          <p>
+            LinkedIn: <a href="https://www.linkedin.com/in/lauerpeter/">LinkedIn-URL</a> |
+            GitHub: <a href="https://github.com/LauerPeter?tab=repositories">GitHub-URL</a> |
           </p>
         </div>
         <div className='intro'>
           <p>
-          Full-Stack web developer eager to begin a programming career. Earned a Full-Stack Web Development certificate from the University of Minnesota Bootcamp as well as an Associate of Arts degree from Colorado Mountain College. The AA degree has provided significant leverage for development on the front-end interface of applications. Keen attention to detail and a passion for building and developing ideas into real-life projects. Constantly eager to problem-solve and think creatively to solve difficult problems.
+            Full-Stack web developer with a strong passion for transforming ideas into real-life projects. Earned Full-Stack Web Development Certificate from the University of Minnesota Bootcamp and an Associate of Arts degree from Colorado Mountain College. Proficient in both front-end and back-end development, I bring keen attention to detail and a problem-solving mindset to every project.
           </p>
         </div>
         <div className='technicalSkills'>
           <h1 className='resumeHeader'>TECHNICAL SKILLS</h1>
           <ul>
-            <li>HTML, Cascading Style Sheets (CSS), Bootstrap (Framework) </li>
+            <li>HTML , Cascading Style Sheets (CSS), Bootstrap (Framework)</li>
             <li>JavaScript, React, Node, jQuery, Express</li>
-            <li>Api, MongoDB, SQL, NoSQL, MySQL2</li>
+            <li>API, MongoDB, SQL, NoSQL, MySQL2</li>
             <li>GitHub, GitLab, Debugging, Project Management</li>
+            <li>Fast learner, team player, communication, writing</li>
           </ul>
         </div>
         <div className='projects'>
           <h1 className='resumeHeader'>PROJECTS</h1>
           <ul>
-            <p className='classBHeader'>Hull & Deck | <a href="https://github.com/bpoole53/group-project-3-fake-store">GitHub Link</a> | <a href="https://hull-and-anchor-a86df0a0dbad.herokuapp.com/">Deployed Link</a></p>
-              <li>Summary: Hull & Deck is a fake ecommerce site for selling watercraft.</li>
-              <li>Role: Back-end development</li>
-              <li>Tools: Mongoose, Mongodb, Express, JavaScript, React, seeding</li>
-            <p className='classBHeader'>nosql-socialnetwork | <a href="https://github.com/LauerPeter/nosql-socialnetwork">GitHub Link</a></p>
-              <li>Summary: API set up for a social network platform.</li>
-              <li>Role: Sole Author</li>
-              <li>Tools: Mongoose, Mongodb, Express, JavaScript </li>
-            <p className='classBHeader'>sql-employee-tracker | <a href="https://github.com/LauerPeter/sql-employee-tracker">GitHub Link</a></p>
-              <li>Summary: View all departments, roles, and employees at any given time. Also update any information on employees etc.</li>
-              <li>Role: Sole Author</li>
-              <li>Tools: Mysql2, Inquirer, Express, JavaScript </li>
+            <p className='classBHeader'>Hull & Deck | <a href="https://github.com/bpoole53/group-project-3-fake-store">GitHub Link</a> | <a href="https://hull-and-anchor-a86df0a0dbad.herokuapp.com/">Deployed Link</a> | Agile group development</p>
+            <li>Summary: Hull & Deck is a fictitious e-commerce site for selling watercraft, showcasing React skills.</li>
+            <li>Development: Designed and developed the back-end using MongoDB as the database and Express.js for routing. Seeded the database with 20 different watercraft ensuring the data accurately represented the products. Created robust routes for manipulating data across the site, allowing users to seamlessly browse and interact with products. Built product and user models and implemented a cart schema attached to the user model, enabling users to add products to their cart. To facilitate data manipulation and interaction, created controllers for cart, product, and user.</li>
+            <li>Tools: MongoDB, Express, JavaScript, React, CSS, Mongoose</li>
+            <p className='classBHeader'>NoSQL-SocialNetwork | <a href="https://github.com/LauerPeter/nosql-socialnetwork">GitHub Link</a> | Sole Author</p>
+            <li>Summary: The NoSQL Social Network API is a robust backend solution for a social networking platform, utilizing MongoDB and Express.js. The back-end application is designed to handle vast amounts of unstructured data efficiently.</li>
+            <li>Development: Uses MongoDB as the primary data storage solution. Designed and implemented thought and user models to represent the core data structures, allowing users to create accounts, share thoughts, react to thoughts, and manage their friend lists. Added a reaction schema to the thought model to enable users to interact with thoughts. Created a set of 15 different routes that facilitated data manipulation throughout the back-end of the application.</li>
+            <li>Tools: MongoDB, Express, Mongoose</li>
+            <p className='classBHeader'>SQL-Employee-Tracker | <a href="https://github.com/LauerPeter/sql-employee-tracker">GitHub Link</a> | Sole Author</p>
+            <li>Summary: Application that utilizes the command line to build and maintain a database for a company using SQL. It runs on inquirer and uses queries to view all departments, roles, and employees, add new departments, roles, and employees, and update employee roles.</li>
+            <li>Development: Utilized SQL to design and create tables corresponding to departments, employees, and roles. Added an intuitive user interface using inquirer prompts, allowing users to view all departments, roles, and employees, add new departments, roles, and employees, and update employee roles with ease. The schema for employees included essential details like ID, first name, last name, role, and a manager ID for that employee.</li>
+            <li>Tools: MySQL2, Inquirer, JavaScript</li>
           </ul>
         </div>
         <div className='experience'>
           <h1 className='resumeHeader'>EXPERIENCE</h1>
-            <p className='classBHeader'> GitHub | June 2023 - Present</p>
+          <p className='classBHeader'>GitHub | June 2023 - Present</p>
           <ul>
-            <li>Refer to GitHub account to see a full list of projects - <a href="https://github.com/LauerPeter">GitHub Link</a></li>
-          </ul>
-            <p className='classBHeader'>Executive Home Management | 2021 - 2023</p>
-          <ul>
-            <li>Property management - abundance of responsibilities for home and lawn.</li>
+            <li>Actively contributing to past projects and creating new projects with different technologies to continue to gain experience.</li>
           </ul>
         </div>
         <div className='education'>
           <h1 className='resumeHeader'>EDUCATION</h1>
-          <p className='classBHeader'>Colorado Mountain College | Associate of Arts (AA)</p>
+          <p className='classBHeader'>University of Minnesota | Full-Stack Web Development Certificate | 2023</p>
           <ul>
-            <li>Writing</li>
-            <li>Communication</li>
+            <li>Skills Developed: Computer Science</li>
           </ul>
-          <p className='classBHeader'>University of Minnesota Bootcamp | Full-Stack Web Development Certificate</p>
+          <p className='classBHeader'>Colorado Mountain College | Associate of Arts (AA) | 2020-2022</p>
           <ul>
-            <li>Full-Stack Web Development</li>
+            <li>Skills Developed: Writing and Communication</li>
           </ul>
         </div>
-         <DocxDownload />
       </div>
     </div>
   );
-}
-
+  }
 
 export default Resume;
